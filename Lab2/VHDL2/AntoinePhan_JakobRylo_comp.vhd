19cc2d4b52816d41b45abceedbb0dd4133a40bf2
notkaramel <hoangtuan11102@gmail.com>
2022-10-23 21:52:36 -0400

    Re-adding Lab1 and Lab2

---
+++ Lab2/VHDL2/AntoinePhan_JakobRylo_comp.vhd
@@ -0,0 +1,61 @@
+-- Copyright (C) 1991-2015 Altera Corporation. All rights reserved.
+-- Your use of Altera Corporation's design tools, logic functions 
+-- and other software and tools, and its AMPP partner logic 
+-- functions, and any output files from any of the foregoing 
+-- (including device programming or simulation files), and any 
+-- associated documen0tation or information are expressly subject 
+-- to the terms and conditions of the Altera Program License 
+-- Subscription Agreement, the Altera Quartus Prime License Agreement,
+-- the Altera MegaCore Function License Agreement, or other 
+-- applicable license agreement, including, without limitation, 
+-- that your use is for the sole purpose of programming logic 
+-- devices manufactured by Altera and sold by Altera or its 
+-- authorized distributors.  Please refer to the applicable 
+-- agreement for further details.
+
+-- PROGRAM		"Quartus Prime"
+-- VERSION		"Version 15.1.0 Build 185 10/21/2015 SJ Standard Edition"
+-- CREATED		"Tue Sep 13 14:56:56 2022"
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all; 
+
+LIBRARY work;
+
+ENTITY AntoinePhan_JakobRylo_comp IS 
+	PORT
+	(
+		A :  IN  STD_LOGIC_VECTOR(3 DOWNTO 0);
+		B :  IN  STD_LOGIC_VECTOR(3 DOWNTO 0);
+		AeqB :  OUT  STD_LOGIC
+	);
+END AntoinePhan_JakobRylo_comp;
+
+ARCHITECTURE bdf_type OF AntoinePhan_JakobRylo_comp IS 
+
+SIGNAL	SYNTHESIZED_WIRE_0 :  STD_LOGIC;
+SIGNAL	SYNTHESIZED_WIRE_1 :  STD_LOGIC;
+SIGNAL	SYNTHESIZED_WIRE_2 :  STD_LOGIC;
+SIGNAL	SYNTHESIZED_WIRE_3 :  STD_LOGIC;
+
+
+BEGIN 
+
+
+
+SYNTHESIZED_WIRE_0 <= NOT(A(0) XOR B(0));
+
+
+SYNTHESIZED_WIRE_1 <= NOT(A(1) XOR B(1));
+
+
+SYNTHESIZED_WIRE_2 <= NOT(A(2) XOR B(2));
+
+
+SYNTHESIZED_WIRE_3 <= NOT(A(3) XOR B(3));
+
+
+AeqB <= SYNTHESIZED_WIRE_0 AND SYNTHESIZED_WIRE_1 AND SYNTHESIZED_WIRE_2 AND SYNTHESIZED_WIRE_3;
+
+
+END bdf_type;
