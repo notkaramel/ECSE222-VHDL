303b53676fd8ee0734d9ee2bb19ce9f6692659f5
notkaramel <hoangtuan11102@gmail.com>
2022-11-05 19:44:32 -0400

    Update Lab5 with fixed code

--- Lab5/VHDL5_Antoine_Phan/antoine_phan_comparator.vhd
+++ Lab5/VHDL5_Antoine_Phan/antoine_phan_comparator.vhd
@@ -15,13 +15,13 @@ end antoine_phan_comparator;
 architecture arch of antoine_phan_comparator is
 signal BplusOne: std_logic_vector(4 downto 0);
 begin
-	comparing: process(A, B, BplusOne)
+	process(A, B, BplusOne)
 	begin
 		-- Add 1 to B, check overflow
 		BplusOne <= ('0' & B) + "00001";
-		-- Comparing A and B
 		
-		if (BplusOne(4) = '1') then
+		-- Comparing A and B
+		if (BplusOne(4) = '1') then -- addition produce overflow
 			overflow <= '1';
 			AeqBplusOne <= '0';
 			AgtBplusOne <= '0';
@@ -29,37 +29,39 @@ begin
 			AltBplusOne <= '0';
 			AlteBplusOne <= '0';
 		else
-			overflow <= '0';
-			if (A = BplusOne) then
-				AeqBplusOne <= '1';
-				AgtBplusOne <= '1';
-				AgteBplusOne <= '1';
-				AltBplusOne <= '0';
-				AlteBplusOne <= '0';
-			elsif (A > BplusOne) then
+			overflow <= '0'; -- no overflow, compare the two numbers
+			if (A > BplusOne) then -- A > B+1 -> 01100
 				AeqBplusOne <= '0';
 				AgtBplusOne <= '1';
 				AgteBplusOne <= '1';
 				AltBplusOne <= '0';
 				AlteBplusOne <= '0';
-			elsif (A >= BplusOne) then
-				AeqBplusOne <= '1';
-				AgtBplusOne <= '0';
-				AgteBplusOne <= '1';
-				AltBplusOne <= '0';
-				AlteBplusOne <= '0';
-			elsif (A < BplusOne) then
+			elsif (A < BplusOne) then -- A < B+1 -> 00011
 				AeqBplusOne <= '0';
 				AgtBplusOne <= '0';
 				AgteBplusOne <= '0';
 				AltBplusOne <= '1';
 				AlteBplusOne <= '1';
-			else --if (A =< BplusOne) then
+			elsif (A = BplusOne) then -- A == B+1 -> 10101
 				AeqBplusOne <= '1';
 				AgtBplusOne <= '0';
-				AgteBplusOne <= '0';
+				AgteBplusOne <= '1';
 				AltBplusOne <= '0';
 				AlteBplusOne <= '1';
+			-- 2 numbers can't be equal and greater at the same time!?
+			-- elsif (A >= BplusOne) then -- A >= B+1 -> 11101???
+				-- AeqBplusOne <= '1';
+				-- AgtBplusOne <= '1';
+				-- AgteBplusOne <= '1';
+				-- AltBplusOne <= '0';
+				-- AlteBplusOne <= '0';
+			
+			-- else --if (A =< BplusOne) then
+			-- 	AeqBplusOne <= '1';
+			-- 	AgtBplusOne <= '0';
+			-- 	AgteBplusOne <= '0';
+			-- 	AltBplusOne <= '0';
+			-- 	AlteBplusOne <= '1';
 			-- else -- illegal states?
 			-- 	AeqBplusOne <= '0';
 			-- 	AgtBplusOne <= '0';
